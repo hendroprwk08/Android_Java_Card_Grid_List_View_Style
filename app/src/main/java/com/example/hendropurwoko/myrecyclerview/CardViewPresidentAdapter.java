f99c8852cbee047419abb0105a85aa4ced33c394
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2019-09-18 13:02:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hendropurwoko/myrecyclerview/CardViewPresidentAdapter.java
@@ -0,0 +1,86 @@
+package com.example.hendropurwoko.myrecyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class CardViewPresidentAdapter extends RecyclerView.Adapter<CardViewPresidentAdapter.CardViewHolder> {
+    private ArrayList<President> listPresident;
+    private Context context;
+
+    public CardViewPresidentAdapter(Context context) {
+        this.context = context;
+    }
+
+    public ArrayList<President> getListPresident() {
+        return listPresident;
+    }
+
+    public void setListPresident(ArrayList listPresident) {
+        this.listPresident = listPresident;
+    }
+
+    @Override
+    public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cardview_president, parent, false);
+        CardViewHolder viewHolder = new CardViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(CardViewHolder holder, int position) {
+        President president = getListPresident().get(position);
+
+        Glide.with(context)
+                .load(president.getPhoto())
+                .override(350, 350)
+                .into(holder.imgPhoto);
+
+        holder.tvName.setText(president.getName());
+        holder.tvRemarks.setText(president.getRemarks());
+
+        holder.btnFavorite.setOnClickListener(new CustomOnItemClickListener(position, new CustomOnItemClickListener.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(View view, int position) {
+                Toast.makeText(context, "Favorite: " + getListPresident().get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        }));
+
+        holder.btnShare.setOnClickListener(new CustomOnItemClickListener(position, new CustomOnItemClickListener.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(View view, int position) {
+                Toast.makeText(context, "Share: " + getListPresident().get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        }));
+    }
+
+    @Override
+    public int getItemCount() {
+        return getListPresident().size();
+    }
+
+    public class CardViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        TextView tvName, tvRemarks;
+        Button btnFavorite, btnShare;
+
+        public CardViewHolder(View view) {
+            super(view);
+            imgPhoto = (ImageView) itemView.findViewById(R.id.img_item_photo);
+            tvName = (TextView) itemView.findViewById(R.id.tv_item_name);
+            tvRemarks = (TextView) itemView.findViewById(R.id.tv_item_remarks);
+            btnFavorite = (Button) itemView.findViewById(R.id.btn_set_favorite);
+            btnShare = (Button) itemView.findViewById(R.id.btn_set_share);
+        }
+    }
+}
